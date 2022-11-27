88725583fc93ab779e7c2740a9fbdb738f2046e6
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-11-27 20:15:31 +0700

    Add template

---
+++ bainopcanhan-main/asset/infoDB.php
@@ -0,0 +1,15 @@
+<?php
+    $servername = "localhost";
+
+    // host
+
+    $username = "id18943592_php";
+    $password = "12345678910Aa@";
+    $dbname = "id18943592_minhtri";
+
+    // localhost xampp
+
+    // $username = "root";
+    // $password = "";
+    // $dbname = "php";
+?>
